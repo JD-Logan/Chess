8aaadd106f0044ddb4f5ab2e685ce3ff1e241923
JD <jdlogan@me.com>
2026-01-29 11:24:33 -0700

    p0 Clean up and shorten code. p1 start.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -52,6 +53,7 @@ public class ChessGame {
      */
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         throw new RuntimeException("Not implemented");
+        // is a piece move AND the move doesn't leave your king in check
     }
 
     /**
@@ -62,6 +64,8 @@ public class ChessGame {
      */
     public void makeMove(ChessMove move) throws InvalidMoveException {
         throw new RuntimeException("Not implemented");
+        // Receives a given move and executes it, provided it is a legal move. If the move is illegal, it throws an InvalidMoveException.
+        // A move is illegal if it is not a "valid" move for the piece at the starting location, or if it’s not the corresponding team's turn.
     }
 
     /**
@@ -72,6 +76,7 @@ public class ChessGame {
      */
     public boolean isInCheck(TeamColor teamColor) {
         throw new RuntimeException("Not implemented");
+        // Returns true if the specified team’s King could be captured by an opposing piece
     }
 
     /**
@@ -82,6 +87,7 @@ public class ChessGame {
      */
     public boolean isInCheckmate(TeamColor teamColor) {
         throw new RuntimeException("Not implemented");
+        // Returns true if the given team has no way to protect their king from being captured.
     }
 
     /**
@@ -93,8 +99,27 @@ public class ChessGame {
      */
     public boolean isInStalemate(TeamColor teamColor) {
         throw new RuntimeException("Not implemented");
+        // Returns true if the given team has no legal moves but their king is not in immediate danger.
     }
 
+    // Extra Credit Moves
+    //If you would like to fully implement the rules of chess you need to provide support for Castling and En Passant.
+    //You do not have to implement these moves, but if you go the extra mile and successfully implement them, you’ll earn 5 extra credit points for each move (10 total) on this assignment.
+
+    // Castling
+    //This is a special move where the King and a Rook move simultaneously. The castling move can only be taken when 3 conditions are met:
+    //
+    //Neither the King nor Rook have moved since the game started
+    //There are no pieces between the King and the Rook
+    //The King is never in Check. The King does not start in Check, does not cross a square on which it would be in Check, and is not in Check after castling.
+    //To Castle, the King moves 2 spaces towards the Rook, and the Rook "jumps" the king moving to the position next to and on the other side of the King.
+    // This is represented in a ChessMove as the king moving 2 spaces to the side.
+
+    // En Passant
+    //This is a special move taken by a Pawn in response to your opponent double moving a Pawn.
+    // If your opponent double moves a pawn so it ends next to yours (skipping the position where your pawn could have captured their pawn),
+    // then on your immediately following turn your pawn may capture their pawn as if their pawn had only moved 1 square.
+    // This is as if your pawn is capturing their pawn mid motion, or "In Passing."
     /**
      * Sets this game's chessboard with a given board
      *
@@ -111,5 +136,19 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return this.board;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(board, chessGame.board);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, board);
     }
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -55,147 +55,99 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-//        ChessPiece piece = board.getPiece(myPosition);
-//        if (piece.getPieceType() == PieceType.BISHOP) {
-//            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null)); // hardcoded
-//        }
-//        return List.of();
-        // Bishop moves idea
-        if (this.type == PieceType.BISHOP) {
-            List<ChessMove> moves = new ArrayList<>();
-            longRangeMoves(board, myPosition, moves, 1, 1); // UP-RIGHT
-            longRangeMoves(board, myPosition, moves, -1, 1); // UP-LEFT
-            longRangeMoves(board, myPosition, moves, 1, -1); // DOWN-RIGHT
-            longRangeMoves(board, myPosition, moves, -1, -1); // DOWN-LEFT
-            return moves;
+        List<ChessMove> moves = new ArrayList<>();
 
-            // ROOK
-        } else if (this.type == PieceType.ROOK) {
-            List<ChessMove> moves = new ArrayList<>();
-            longRangeMoves(board, myPosition, moves, 1, 0); // UP
-            longRangeMoves(board, myPosition, moves, -1, 0); // DOWN
-            longRangeMoves(board, myPosition, moves, 0, -1); // LEFT
-            longRangeMoves(board, myPosition, moves, 0, 1); // RIGHT
-
-            return moves;
-
-            // QUEEN
+        if (this.type == PieceType.ROOK) {
+            slidingRangeMove(board, myPosition, moves, 1,0);
+            slidingRangeMove(board, myPosition, moves, 0,1);
+            slidingRangeMove(board, myPosition, moves, -1,0);
+            slidingRangeMove(board, myPosition, moves, 0,-1);
+        } else if (this.type == PieceType.BISHOP) {
+            slidingRangeMove(board, myPosition, moves, 1,1);
+            slidingRangeMove(board, myPosition, moves, -1,1);
+            slidingRangeMove(board, myPosition, moves, -1,-1);
+            slidingRangeMove(board, myPosition, moves, 1,-1);
         } else if (this.type == PieceType.QUEEN) {
-            List<ChessMove> moves = new ArrayList<>();
-            longRangeMoves(board, myPosition, moves, 1, 0); // UP
-            longRangeMoves(board, myPosition, moves, -1, 0); // DOWN
-            longRangeMoves(board, myPosition, moves, 0, -1); // LEFT
-            longRangeMoves(board, myPosition, moves, 0, 1); // RIGHT
-            longRangeMoves(board, myPosition, moves, 1, 1); // UP-RIGHT
-            longRangeMoves(board, myPosition, moves, -1, 1); // UP-LEFT
-            longRangeMoves(board, myPosition, moves, 1, -1); // DOWN-RIGHT
-            longRangeMoves(board, myPosition, moves, -1, -1); // DOWN-LEFT
-
-            return moves;
-
+            slidingRangeMove(board, myPosition, moves, 1,0);
+            slidingRangeMove(board, myPosition, moves, 0,1);
+            slidingRangeMove(board, myPosition, moves, -1,0);
+            slidingRangeMove(board, myPosition, moves, 0,-1);
+            slidingRangeMove(board, myPosition, moves, 1,1);
+            slidingRangeMove(board, myPosition, moves, -1,1);
+            slidingRangeMove(board, myPosition, moves, -1,-1);
+            slidingRangeMove(board, myPosition, moves, 1,-1);
         } else if (this.type == PieceType.KING) {
-            List<ChessMove> moves = new ArrayList<>();
-
-            singleRangeMove(board, myPosition, moves,1,-1); // UP-LEFT
-            singleRangeMove(board, myPosition, moves,1,0); // UP
-            singleRangeMove(board, myPosition, moves,1,1); // UP-RIGHT
-            singleRangeMove(board, myPosition, moves,0,-1); // LEFT
-            singleRangeMove(board, myPosition, moves,0,1); // RIGHT
-            singleRangeMove(board, myPosition, moves,-1,-1); // DOWN-LEFT
-            singleRangeMove(board, myPosition, moves,-1,0); // DOWN
-            singleRangeMove(board, myPosition, moves,-1,1); // DOWN-RIGHT
-
-            return moves;
+            singleRangeMove(board, myPosition, moves, 1,0);
+            singleRangeMove(board, myPosition, moves, 0,1);
+            singleRangeMove(board, myPosition, moves, -1,0);
+            singleRangeMove(board, myPosition, moves, 0,-1);
+            singleRangeMove(board, myPosition, moves, 1,1);
+            singleRangeMove(board, myPosition, moves, -1,1);
+            singleRangeMove(board, myPosition, moves, -1,-1);
+            singleRangeMove(board, myPosition, moves, 1,-1);
         } else if (this.type == PieceType.KNIGHT) {
-            List<ChessMove> moves = new ArrayList<>();
-
-            singleRangeMove(board, myPosition, moves,1,-2); // LEFT-UP
-            singleRangeMove(board, myPosition, moves,2,-1); // UP-LEFT
-            singleRangeMove(board, myPosition, moves,2,1); // UP-RIGHT
-            singleRangeMove(board, myPosition, moves,1,2); // RIGHT-UP
-            singleRangeMove(board, myPosition, moves,-1,2); // RIGHT_DOWN
-            singleRangeMove(board, myPosition, moves,-2,1); // DOWN-RIGHT
-            singleRangeMove(board, myPosition, moves,-2,-1); // DOWN-LEFT
-            singleRangeMove(board, myPosition, moves,-1,-2); // LEFT-DOWN
-
-            return moves;
-
-
-            // PAWN
-            // NOT COMPLETE
-            // NOTE: pawns can only move one direction which is gonna make this weird because white and black need to go their ways. so set a flag for white or black to determine the direction.
-            // also probably just do knight and king and do the pawn last.
+            singleRangeMove(board, myPosition, moves, 1,2);
+            singleRangeMove(board, myPosition, moves, 2,1);
+            singleRangeMove(board, myPosition, moves, 2,-1);
+            singleRangeMove(board, myPosition, moves, 1,-2);
+            singleRangeMove(board, myPosition, moves, -1,-2);
+            singleRangeMove(board, myPosition, moves, -2,-1);
+            singleRangeMove(board, myPosition, moves, -2,1);
+            singleRangeMove(board, myPosition, moves, -1,2);
         } else if (this.type == PieceType.PAWN) {
-            List<ChessMove> moves = new ArrayList<>();
+            int teamDirection = (ChessGame.TeamColor.WHITE == this.pieceColor) ? 1 : -1;
+            int pawnFirstMove = (ChessGame.TeamColor.WHITE == this.pieceColor) ? 2 : 7;
 
-            int row = myPosition.getRow();
+            int rowAhead = myPosition.getRow() + teamDirection;
             int col = myPosition.getColumn();
 
-            // the direction part of this
-            int teamDirection = (this.pieceColor == ChessGame.TeamColor.WHITE) ? 1 : -1;
-            int pawnFirstMove = (this.pieceColor == ChessGame.TeamColor.WHITE) ? 2 : 7;
+            if (rowAhead >= 1 && rowAhead <= 8 && col >= 1 && col <= 8) {
+                ChessPosition posAhead = new ChessPosition(rowAhead, col);
+                ChessPiece pieceAtPosAhead = board.getPiece(posAhead);
 
-            int rowAhead = row + teamDirection;
-
-            if (rowAhead >= 1 && rowAhead <= 8) {
-                ChessPosition aheadPos = new ChessPosition(rowAhead, col);
-                ChessPiece pieceAtAheadPos = board.getPiece(aheadPos);
-
-                if (pieceAtAheadPos == null) {
-                    // Promotions
+                if (pieceAtPosAhead == null) {
                     if (rowAhead == 8 || rowAhead == 1) {
-                        moves.add(new ChessMove(myPosition, aheadPos, PieceType.QUEEN));
-                        moves.add(new ChessMove(myPosition, aheadPos, PieceType.ROOK));
-                        moves.add(new ChessMove(myPosition, aheadPos, PieceType.BISHOP));
-                        moves.add(new ChessMove(myPosition, aheadPos, PieceType.KNIGHT));
+                        moves.add(new ChessMove(myPosition, posAhead, PieceType.QUEEN));
+                        moves.add(new ChessMove(myPosition, posAhead, PieceType.KNIGHT));
+                        moves.add(new ChessMove(myPosition, posAhead, PieceType.BISHOP));
+                        moves.add(new ChessMove(myPosition, posAhead, PieceType.ROOK));
                     } else {
-                        moves.add(new ChessMove(myPosition, aheadPos, null));
+                        moves.add(new ChessMove(myPosition, posAhead, null));
                     }
+                    if (myPosition.getRow() == pawnFirstMove) {
+                        ChessPosition posTwoAhead = new ChessPosition(rowAhead + teamDirection, col);
+                        ChessPiece pieceAtTwoAhead = board.getPiece(posTwoAhead);
 
-                    // first move double space move
-                    if (row == pawnFirstMove) {
-                    int twoRowAhead = row + (2 * teamDirection);
-                    ChessPosition twoAheadPos = new ChessPosition(twoRowAhead, col);
-                    ChessPiece pieceTwoAhead = board.getPiece(twoAheadPos);
-
-                        if (pieceTwoAhead == null) {
-                            moves.add(new ChessMove(myPosition, twoAheadPos, null));
+                        if (pieceAtTwoAhead == null) {
+                            moves.add(new ChessMove(myPosition, posTwoAhead, null));
                         }
                     }
                 }
             }
-            // diagonal capture
-            List<Integer> diagonalCapture = List.of(-1,1);
-            for (int i = 0; i < diagonalCapture.size(); i++) {
-                int captureCol = col + diagonalCapture.get(i);
 
-                if (rowAhead >=1 && rowAhead <=8 && captureCol >=1 && captureCol <=8) {
-                    ChessPosition capturePos = new ChessPosition(rowAhead, captureCol);
-                    ChessPiece pieceAtPos = board.getPiece(capturePos);
+            List<Integer> diagonalCapture = List.of(-1,1);
+            for (Integer integer : diagonalCapture) {
+                int capCol = col + integer;
+
+                if (rowAhead >= 1 && rowAhead <= 8 && capCol >= 1 && capCol <= 8) {
+                    ChessPosition newPos = new ChessPosition(rowAhead, capCol);
+                    ChessPiece pieceAtPos = board.getPiece(newPos);
 
                     if (pieceAtPos != null && pieceAtPos.getTeamColor() != this.pieceColor) {
-
                         if (rowAhead == 8 || rowAhead == 1) {
-                            moves.add(new ChessMove(myPosition, capturePos, PieceType.QUEEN));
-                            moves.add(new ChessMove(myPosition, capturePos, PieceType.ROOK));
-                            moves.add(new ChessMove(myPosition, capturePos, PieceType.BISHOP));
-                            moves.add(new ChessMove(myPosition, capturePos, PieceType.KNIGHT));
+                            moves.add(new ChessMove(myPosition, newPos, PieceType.QUEEN));
+                            moves.add(new ChessMove(myPosition, newPos, PieceType.KNIGHT));
+                            moves.add(new ChessMove(myPosition, newPos, PieceType.BISHOP));
+                            moves.add(new ChessMove(myPosition, newPos, PieceType.ROOK));
                         } else {
-                            moves.add(new ChessMove(myPosition, capturePos, null));
+                            moves.add(new ChessMove(myPosition, newPos, null));
                         }
                     }
-
                 }
             }
-
-
-
-            return moves;
-
-        } else {
-            return List.of();
         }
 
+        return moves;
     }
 
     private void singleRangeMove(ChessBoard board, ChessPosition myPosition, List<ChessMove> moves, int rowChange, int colChange) {
@@ -212,7 +164,7 @@ public class ChessPiece {
         }
     }
 
-    private void longRangeMoves(ChessBoard board, ChessPosition myPosition, List<ChessMove> moves, int rowChange, int colChange) {
+    private void slidingRangeMove(ChessBoard board, ChessPosition myPosition, List<ChessMove> moves, int rowChange, int colChange) {
         int row = myPosition.getRow() + rowChange;
         int col = myPosition.getColumn() + colChange;
 
