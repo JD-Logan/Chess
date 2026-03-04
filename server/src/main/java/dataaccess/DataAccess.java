b611acbf7ce05a9bbbecbbc1163ed9467c8468ae
JD <jdlogan@me.com>
2026-03-03 20:57:43 -0700

    p3 DataAccess interface

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,28 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+
+    void clear() throws DataAccessException;
+
+
+    void createUser(UserData user) throws DataAccessException;
+    UserData getUser(String username) throws DataAccessException;
+
+
+    GameData createGame(String gameName) throws DataAccessException;
+    GameData getGame(int gameID) throws DataAccessException;
+    Collection<GameData> listGames() throws DataAccessException;
+    void updateGame(GameData game) throws DataAccessException;
+
+
+    AuthData createAuth(String username) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+
+}
