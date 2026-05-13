75422a7cfcf4176ca4c6f4e63fd733225e3336dd
JD <jdlogan@me.com>
2026-05-13 15:06:49 -0600

    p3 Tests finished

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,7 +1,13 @@
 package service;
 
+import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+
+import java.util.Collection;
 
 public class GameServiceTest {
 
@@ -19,20 +25,112 @@ public class GameServiceTest {
     }
 
     @Test
-    void listGamesSuccess() {}
+    void listGamesSuccess() throws DataAccessException {
+        AuthData auth = userService.register(new UserData("JD", "password", "email@me.com"));
+        String token = auth.authToken();
+        Collection<GameData> games = gameService.listGames(token);
+
+        // assert games start empty
+        Assertions.assertTrue(games.isEmpty());
+
+        // assert adding a game makes not empty
+        gameService.createGame(token, "Test Game");
+        Collection<GameData> afterCreateGames = gameService.listGames(token);
+
+        Assertions.assertEquals(1,afterCreateGames.size());
+
+        // assert added game name is in list
+        GameData game = afterCreateGames.iterator().next();
+        Assertions.assertEquals("Test Game", game.gameName());
+
+
+    }
 
     @Test
-    void listGamesFailure() {}
+    void listGamesFailure() {
+        Assertions.assertThrows(SecurityException.class, () ->
+                gameService.listGames("Bad Token"));
+    }
 
     @Test
-    void createGameSuccess() {}
+    void createGameSuccess() throws DataAccessException {
+        AuthData auth = userService.register(new UserData("JD", "password", "email@me.com"));
+        String token = auth.authToken();
+        GameData game = gameService.createGame(token, "Test Game");
+
+        Assertions.assertEquals(1, game.gameID());
+        Assertions.assertEquals("Test Game", game.gameName());
+        Assertions.assertNotNull(dataAccess.getGame(1));
+    }
 
     @Test
-    void createGameFailure() {}
+    void createGameFailure() throws DataAccessException {
+        AuthData auth = userService.register(new UserData("JD", "password", "email@me.com"));
+        String token = auth.authToken();
+        GameData game = gameService.createGame(token, "Test Game");
+
+        Assertions.assertEquals(1, game.gameID());
+        Assertions.assertEquals("Test Game", game.gameName());
+        Assertions.assertNotNull(dataAccess.getGame(1));
+
+        // invalid authToken
+        Assertions.assertThrows(SecurityException.class, () ->
+                gameService.createGame("Bad Token", "Test Game 2")
+        );
+
+        // invalid gameName
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                gameService.createGame(token, null)
+        );
+    }
 
     @Test
-    void joinGameSuccess() {}
+    void joinGameSuccess() throws DataAccessException {
+        AuthData auth1 = userService.register(new UserData("JD", "password", "email@me.com"));
+        String token1 = auth1.authToken();
+        GameData game = gameService.createGame(token1, "Test Game");
+        AuthData auth2 = userService.register(new UserData("Taylor", "password", "email@me.com"));
+        String token2 = auth2.authToken();
+
+        // White join
+        gameService.joinGame(token1, "WHITE", game.gameID());
+        GameData updatedGame1 = dataAccess.getGame(game.gameID());
+        Assertions.assertEquals("JD", updatedGame1.whiteUsername());
+
+        // Black join
+        gameService.joinGame(token2, "BLACK", game.gameID());
+        GameData updatedGame2 = dataAccess.getGame(game.gameID());
+        Assertions.assertEquals("Taylor", updatedGame2.blackUsername());
+
+    }
 
     @Test
-    void joinGameFailure() {}
+    void joinGameFailure() throws DataAccessException {
+        AuthData auth1 = userService.register(new UserData("JD", "password", "email@me.com"));
+        String token1 = auth1.authToken();
+        GameData game = gameService.createGame(token1, "Test Game");
+        AuthData auth2 = userService.register(new UserData("Taylor", "password", "email@me.com"));
+        String token2 = auth2.authToken();
+
+        // join with bad authToken
+        Assertions.assertThrows(SecurityException.class, () ->
+                gameService.joinGame("Bad Token", "WHITE", game.gameID())
+        );
+
+        // White join twice
+        gameService.joinGame(token1, "WHITE", game.gameID());
+        Assertions.assertThrows(IllegalStateException.class, () ->
+                gameService.joinGame(token2, "WHITE", game.gameID())
+        );
+
+        // invalid color join
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                gameService.joinGame(token1, "GREY", game.gameID())
+        );
+
+        // invalid gameID join
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                gameService.joinGame(token1, "WHITE", 999)
+        );
+    }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,7 +1,12 @@
 package service;
 
+import dataaccess.DataAccessException;
 import dataaccess.MemoryDataAccess;
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.*;
+
+import java.util.Objects;
 
 public class UserServiceTest {
 
@@ -18,21 +23,113 @@ public class UserServiceTest {
         gameService = new GameService(dataAccess);
     }
     @Test
-    void registerSuccess() {}
+    void registerSuccess() throws DataAccessException {
+        AuthData auth = userService.register(new UserData("JD", "password", "email@me.com"));
+        String token = auth.authToken();
+
+        Assertions.assertNotNull(dataAccess.getUser("JD"));
+        Assertions.assertNotNull(dataAccess.getAuth(token));
+
+    }
 
     @Test
-    void registerFailure() {}
+    void registerFailure_duplicateUsername() throws DataAccessException {
+
+//        AuthData auth = userService.register(new UserData("JD", "password", "email@me.com"));
+//        String token1 = auth.authToken();
+//
+//        AuthData auth2 = userService.register(new UserData("JD", "password2", "email2@me.com"));
+//        String token2 = auth2.authToken();
+//
+//        Assertions.assertNull(dataAccess.getUser("JD"));
+//        Assertions.assertNull(dataAccess.getAuth(token2));
+
+//        Assertions.assertThrows(IllegalStateException.class());
+
+        // duplicate usernames
+
+        // register a new user1
+        userService.register(new UserData("JD", "password", "email@me.com"));
+        // Assert assertThrows the exception
+        Assertions.assertThrows(IllegalStateException.class, () ->
+                userService.register(new UserData("JD", "password2", "email2@me.com"))
+        );
+                // register a user2
+
+        // missing fields
+        //
+    }
 
     @Test
-    void loginSuccess() {}
+    void registerFailure_missingFields() throws DataAccessException {
+        // missing username
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                userService.register(new UserData(null, "password", "email@me.com"))
+        );
+        // missing password
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                userService.register(new UserData("JD", null, "email@me.com"))
+        );
+        // missing email
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                userService.register(new UserData("JD", "password", null))
+        );
+    }
+
 
     @Test
-    void loginFailure() {}
+    void loginSuccess() throws DataAccessException {
+        userService.register(new UserData("JD", "password", "email@me.com"));
+        AuthData auth = userService.login("JD", "password");
+
+        Assertions.assertNotNull(auth.authToken());
+        Assertions.assertEquals("JD", auth.username());
+        Assertions.assertNotNull(dataAccess.getAuth(auth.authToken()));
+    }
 
     @Test
-    void logoutSuccess() {}
+    void loginFailure() throws DataAccessException {
+        // can split into three tests if I want more specific error help
+        // bad password
+        userService.register(new UserData("JD", "password", "email@me.com"));
+        // assert
+        Assertions.assertThrows(SecurityException.class, () ->
+                // login with bad password
+                userService.login("JD", "password1")
+        );
+
+        // unknown user
+        Assertions.assertThrows(SecurityException.class, () ->
+                userService.login("Not JD", "password")
+        );
+
+        // null fields (username)
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                userService.login(null, "password")
+        );
+
+        // null fields (pw)
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                userService.login("Not JD", null)
+        );
+    }
 
     @Test
-    void logoutFailure() {}
+    void logoutSuccess() throws DataAccessException {
+        userService.register(new UserData("JD", "password", "email@me.com"));
+        AuthData auth = userService.login("JD", "password");
+        userService.logout(auth.authToken());
+
+        Assertions.assertNull(dataAccess.getAuth(auth.authToken()));
+    }
+
+    @Test
+    void logoutFailure() throws DataAccessException {
+//        userService.logout("Fake Token");
+
+        Assertions.assertThrows(SecurityException.class, () ->
+                userService.logout("Fake Token")
+        );
+    }
 
 }
