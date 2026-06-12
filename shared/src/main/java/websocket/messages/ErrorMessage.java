5c41c7092981d860b2691374c2629e2950f4b3c7
JD <jdlogan@me.com>
2026-06-12 00:14:15 -0600

    p6 loadgame, error, notification message set up

--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -1,4 +1,16 @@
 package websocket.messages;
 
-public class ErrorMessage {
+import model.GameData;
+
+public class ErrorMessage extends ServerMessage {
+    private final String errorMessage;
+
+    public ErrorMessage(String errorMessage) {
+        super(ServerMessageType.ERROR);
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
